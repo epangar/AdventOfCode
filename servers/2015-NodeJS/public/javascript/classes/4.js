294656f7612d134539fb49c15201e87d2fd19c49
epangar <e.pan.gar@gmail.com>
2022-10-07 01:25:15 +0200

    Solution4 class finished

---
+++ servers/2015-NodeJS/public/javascript/classes/4.js
@@ -0,0 +1,44 @@
+/* 'use strict'; */
+const getFilePaths = require('../utils/getFilePaths');
+const fillInput = require('../utils/fillInput');
+const md5 = require('md5');
+
+
+class Solution4 {
+    constructor(){
+        this.filePathExample = getFilePaths(4).filePathExample;
+        this.filePath1 = getFilePaths(4).filePath1;
+        this.inputExample = fillInput(this.filePathExample, this, 'inputExample')
+        .then(() => {
+            this.example1 = this.calculateFirst(this.inputExample, '00000');
+            this.example2 = this.calculateSecond(this.inputExample, '000000');
+        });
+        
+        this.input = fillInput(this.filePath1, this, 'input')
+        .then(() => {
+            this.part1 = this.calculateFirst(this.input, '00000');
+            this.part2 = this.calculateSecond(this.input, '000000');
+        });
+
+    }
+
+    calculateFirst(input, zeroes){
+        let counter = 1;
+        console.log("arg ", arguments)   
+        while (counter++) {		
+            
+            let hash = md5(input + counter);
+            
+            if(hash.slice(0, zeroes.length) === zeroes){
+                console.log(hash, hash.slice(0, zeroes.length))
+                return counter
+            }
+        }
+    }
+
+    calculateSecond(inputString, numberOfZeroes){
+        return this.calculateFirst(inputString, numberOfZeroes);
+    }
+}
+
+module.exports = Solution4
