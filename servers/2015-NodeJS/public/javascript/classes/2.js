bd31858edc64bae6453785f948a97b7aaeb39722
epangar <e.pan.gar@gmail.com>
2022-10-06 12:20:55 +0200

    Building Solution2 class

---
+++ servers/2015-NodeJS/public/javascript/classes/2.js
@@ -0,0 +1,16 @@
+class Solution2 {
+    constructor(){
+        this.part1 = 900 || this.calculateFirst();
+        this.part2 = 1 || this.calculateSecond();
+    }
+
+    calculateFirst(){
+
+    }
+
+    calculateSecond(){
+        
+    }
+}
+
+module.exports = Solution2
