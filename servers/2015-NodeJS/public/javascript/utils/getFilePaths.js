45df72823cffed262a371a8de52c80060c242098
epangar <e.pan.gar@gmail.com>
2022-10-06 20:12:39 +0200

    Fix getFIlePth.js

--- servers/2015-NodeJS/public/javascript/utils/getFilePaths.js
+++ servers/2015-NodeJS/public/javascript/utils/getFilePaths.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = function(numberOfDay){
     return {
         filePathExample : path.join(process.cwd(), `/public/inputs/${numberOfDay}.example.txt`),
-        filePath1 : path.join(process.cwd(), `/public/inputs/${numberOfDay}.1.txt`),
-        filePath2 : path.join(process.cwd(), `/public/inputs/${numberOfDay}.2.txt`)
+        filePath1 : path.join(process.cwd(), `/public/inputs/${numberOfDay}.txt`)
     }
 }
