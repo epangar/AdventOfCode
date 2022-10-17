a4037953cdfef06a24be1b77b78ee82433334513
epangar <e.pan.gar@gmail.com>
2022-10-17 20:41:38 +0200

    Solution class refactor

--- servers/2015-NodeJS/public/javascript/classes/Solution.js
+++ servers/2015-NodeJS/public/javascript/classes/Solution.js
@@ -1,27 +1,12 @@
-const Solution1 = require('./1');
-const Solution2 = require('./2');
-const Solution3 = require('./3');
-const Solution4 = require('./4');
-const Solution5 = require('./5');
+const imports = require('../utils/Imports')
 
 class Solution {
     constructor(){
+        this.year = 2015;
     }
 
-    get day1(){
-        return new Solution1()
-    }
-    get day2(){
-        return new Solution2()
-    }
-    get day3(){
-        return new Solution3()
-    }
-    get day4(){
-        return new Solution4()
-    }
-    get day5(){
-        return new Solution5()
+    getDay(nInput){
+        return imports[nInput];
     }
 }
 
