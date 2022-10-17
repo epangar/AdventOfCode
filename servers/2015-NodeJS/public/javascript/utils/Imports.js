19f71f5890100ac4c1c63846a8b8073470e4f105
epangar <e.pan.gar@gmail.com>
2022-10-17 20:41:23 +0200

    Imports cache

---
+++ servers/2015-NodeJS/public/javascript/utils/Imports.js
@@ -0,0 +1,18 @@
+const Solution1 = require('../classes/1');
+const Solution2 = require('../classes/2');
+const Solution3 = require('../classes/3');
+const Solution4 = require('../classes/4');
+const Solution5 = require('../classes/5');
+
+
+const Imports  = {
+    
+    day1 : new Solution1(),
+    day2 : new Solution2(),
+    day3 : new Solution3(),
+    day4 : new Solution4(),
+    day5 : new Solution5()
+    
+}
+
+module.exports = Imports
