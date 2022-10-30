0d7b4e02022570052378ad0f66ac88385f186911
epangar <e.pan.gar@gmail.com>
2022-10-30 20:56:07 +0100

    App Module ampliation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SolutionService } from './services/solution.service';
+import { FormComponent } from './components/form/form.component';
+import { ResultsComponent } from './components/results/results.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    //SolutionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
