c87d00f9d1685e80ecf3215434ea07b7186c5000
epangar <e.pan.gar@gmail.com>
2022-10-30 20:55:22 +0100

    Imports.js update

--- servers/2015-NodeJS/public/javascript/utils/Imports.js
+++ servers/2015-NodeJS/public/javascript/utils/Imports.js
@@ -3,6 +3,7 @@ const Solution2 = require('../classes/2');
 const Solution3 = require('../classes/3');
 const Solution4 = require('../classes/4');
 const Solution5 = require('../classes/5');
+const Solution6 = require('../classes/6');
 
 
 const Imports  = {
@@ -11,7 +12,8 @@ const Imports  = {
     day2 : new Solution2(),
     day3 : new Solution3(),
     day4 : new Solution4(),
-    day5 : new Solution5()
+    day5 : new Solution5(),
+    day6 : new Solution6()
     
 }
 
