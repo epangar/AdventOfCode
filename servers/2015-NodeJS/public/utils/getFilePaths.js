725fc4ccfa91b2176402d447c41db70768068c15
epangar <e.pan.gar@gmail.com>
2023-01-21 23:54:49 +0100

    getFilePath.js ampliation

--- servers/2015-NodeJS/public/utils/getFilePaths.js
+++ servers/2015-NodeJS/public/utils/getFilePaths.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
-module.exports = function(numberOfDay){
+module.exports = function(year, numberOfDay){
     return {
-        filePathExample : path.join(process.cwd(), `/public/inputs/${numberOfDay}.example.txt`),
-        filePath1 : path.join(process.cwd(), `/public/inputs/${numberOfDay}.txt`)
+        filePathExample : path.join(process.cwd(), `/public/inputs/${year}/${numberOfDay}.example.txt`),
+        filePath1 : path.join(process.cwd(), `/public/inputs/${year}/${numberOfDay}.txt`)
     }
 }
