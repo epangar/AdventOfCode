7180af0a984cd2890ce1533aa647d3adf6d23ee5
epangar <e.pan.gar@gmail.com>
2023-01-21 23:01:07 +0100

    Fixing Day 6

--- servers/2015-NodeJS/public/javascript/classes/6.js
+++ servers/2015-NodeJS/public/javascript/classes/6.js
@@ -11,14 +11,18 @@ class Light{
     }
 
     turnOff(){
-        if(this.lit){
+        if(this.lit === true){
             this.lit = false;
-        }        
+        } else {
+            return;
+        }       
     }
 
     turnOn(){
         if(!this.lit){
             this.lit = true;
+        } else {
+            return;
         }
     }
 }
@@ -31,22 +35,25 @@ class Solution6 {
         this.dummyGrid = this.makeGrid(5,5);
         //this.grid = this.makeGrid(1000,1000);
         
-        //this.inputExample = fillInput(this.filePathExample, this, 'inputExample')
-        /* .then(() => {
-            //this.inputExample = this.parseInput(this.inputExample);
+        this.inputExample = fillInput(this.filePathExample, this, 'inputExample')
+        .then(() => {
+            this.inputExample = this.parseInput(this.inputExample);
             //console.log("this.inputExample = ",this.inputExample)
-            //const GRID = this.makeGrid(5, 5)
-            //this.example1 =  this.calculateFirst(GRID, this.inputExample);
-        }); */
+            const GRID = this.makeGrid(5, 5)
+            this.example1 =  this.calculateFirst(GRID, this.inputExample);
+        });
         
         
-       this.input = fillInput(this.filePath1, this, 'input')
+       /* this.input = fillInput(this.filePath1, this, 'input')
         .then(() => {
             this.input = this.parseInput(this.input);
-            const GRID = this.makeGrid(1000, 1000);
-            this.part1 = this.calculateFirst(GRID, this.input);
-            /* this.part2 = this.calculateSecond(this.input); */
-        });
+            this.GRID = this.makeGrid(1000, 1000);
+            //console.log(this.input)
+            //console.log(GRID)
+            this.part1 = this.calculateFirst(this.GRID, this.input);
+            console.log(this.part1)
+            /* this.part2 = this.calculateSecond(this.input); 
+        }); */
     }
 
     parseInput(str){
@@ -77,57 +84,46 @@ class Solution6 {
     }
 
     calculateFirst(grid, instructions){
-        console.log("start grid = ", grid)
+        //console.log("start grid = ", grid)
         //we must return .map() of instructions
-        instructions.forEach((instruction, numberOfInstruction) => {
-            const start = instruction.start, 
-                end = instruction.end,
+        instructions.forEach((instruction) => {
+
+            let startH = instruction.start[1], 
+                startV = instruction.start[0], 
+                endH = instruction.end[1],
+                endV = instruction.end[0],
                 method = instruction.method;
-            
-                //console.log("grid = ", grid)
 
-            grid = grid.map((row, numberOfRow) => 
-                row.map((columnLight, numberOfColumn) => {
- 
-                    //let currentPoint = [numberOfRow, numberOfColumn]; //This is where we are
+            for(let i = startH; i <= endH; i++){
+                for(let j = startV; j <= endV; j++){
 
-                    let temp;
+                    //console.log(`Changing:\n [${i}][${j}] = ${grid[i][j].lit}`)
+                    
+                    if(method === 'toggle'){
+                        grid[i][j].toggle();
+                    } else if (method === 'turn off'){
+                        grid[i][j].turnOff();
+                    } else {
+                        grid[i][j].turnOn();
+                    } 
 
-                    if(start[0] > end[0]){
-                        temp = end[0];
-                        end[0] = start[0]
-                        start[0] = temp;
-                    }
-
-                    if(start[1] > end[1]){
-                        temp = end[1];
-                        end[1] = start[1]
-                        start[1] = temp;
-                    }
-                    if(numberOfRow >= start[0]
-                        && numberOfRow <= end[0]
-                        && numberOfColumn >= start[1]
-                        && numberOfColumn <= end[1] ){
-                            console.log("inside ")
-                            console.log(`current ${[numberOfRow, numberOfColumn]}, start ${start}, end ${end}`)
-                            console.log(columnLight, method)
-                            if(method === 'toggle'){
-                                columnLight.toggle();
-                            } else if (method === 'turn off'){
-                                columnLight.turnOff();
-                            } else {
-                                columnLight.turnOn();
-                            }
-                            console.log(columnLight)
-                        }
-
-                    return columnLight
-                })
-            )
+                    //console.log(`Changed:\n [${i}][${j}] = ${grid[i][j].lit}`)
+                }
+            }
         })
+
         
-        console.log("answer so far = ", grid.map(row => row.filter(light => light.lit === true).length))
-        return grid.map(row => row.filter(light => light.lit).length).reduce((a,b)=>a+b)
+        /* const ANSWER = grid.map(row => row.filter(light => light.lit === true).length)
+        
+        console.log("answer so far = ", ANSWER)
+        return ANSWER.reduce((a,b)=>a+b) */
+
+        
+
+        let ANSWER = grid.reduce((count, row)=>{
+            return count + row.filter((l) => l.lit).length
+        },0)
+        return ANSWER
     }
 
     calculateSecond(grid, instructions){
